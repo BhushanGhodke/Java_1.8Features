a76ee4b6ab7b4b25ec73a06d4af9ac311f001af1
BhushanGhodke <bhushanghodke1500@gmail.com>
2023-01-11 23:13:17 +0530

    Lambda fetaure add1

--- src/com/functional/interfacess/Test.java
+++ src/com/functional/interfacess/Test.java
@@ -1,5 +1,16 @@
 package com.functional.interfacess;
 
+@FunctionalInterface
+interface Calculator {
+
+	public void switchOn();
+}
+
 public class Test {
 
+	public static void main(String[] args) {
+		Calculator cal= ()->System.out.println("Switch on");
+		cal.switchOn();
+	}
+	
 }
